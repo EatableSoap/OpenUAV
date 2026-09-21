7ad4c2c45583158d41e7d4d1cef81d89bbe32137
EateableSoap <2646319600@q.com>
2026-09-21 15:44:03 +0800

    Our new reproduce expriments

--- scripts/dagger_NYC.sh
+++ scripts/dagger_NYC.sh
@@ -4,12 +4,12 @@
 root_dir=. # TravelUAV directory
 model_dir=$root_dir/Model/LLaMA-UAV
 
-
-CUDA_VISIBLE_DEVICES=0 python -u $root_dir/src/vlnce_src/dagger.py \
+CUDA_VISIBLE_DEVICES=0 
+python -u $root_dir/src/vlnce_src/dagger.py \
     --run_type collect \
     --collect_type dagger \
     --name TravelLLM \
-    --gpu_id 4 \
+    --gpu_id 0 \
     --simulator_tool_port 25000 \
     --DDP_MASTER_PORT 80002 \
     --batchSize 1 \
@@ -17,7 +17,7 @@ CUDA_VISIBLE_DEVICES=0 python -u $root_dir/src/vlnce_src/dagger.py \
     --dagger_p 0.4 \
     --maxWaypoints 200 \
     --activate_maps NYCEnvironmentMegapa \
-    --dataset_path /mnt/data5/airdrone/dataset/replay_data_log0.1_image0.5/ \
+    --dataset_path $root_dir/data/train_dataset/ \
     --dagger_save_path $root_dir/data/dagger_data \
     --model_path $model_dir/work_dirs/llama-vid-7b-pretrain-224-uav-full-data-lora32 \
     --model_base $model_dir/model_zoo/vicuna-7b-v1.5 \
